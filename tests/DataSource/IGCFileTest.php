ccedc580a57eef7f2ffeb6987b1d6508b9760a54
Chris Emerson <chris@cemerson.co.uk>
2014-08-21 23:40:51 +0100

    Test for non existent igc file throwing exception

--- tests/DataSource/IGCFileTest.php
+++ tests/DataSource/IGCFileTest.php
@@ -2,23 +2,43 @@
 namespace CEmerson\IGC\DataSource;
 
 use org\bovigo\vfs\vfsStream;
+use org\bovigo\vfs\vfsStreamDirectory;
 use org\bovigo\vfs\vfsStreamFile;
 
 class IGCFileTest extends \PHPUnit_Framework_TestCase
 {
+    /** @var vfsStreamDirectory */
+    private $directory;
+
     const TEST_CONTENT = "some test\ncontent\n\n";
 
-    public function testIGCFile()
+    public function setUp()
     {
-        $directory = vfsStream::setup('test');
-        $testIGCFIle = new vfsStreamFile("test.igc");
+        $this->directory = vfsStream::setup('test');
+    }
 
-        $testIGCFIle
+    public function tearDown()
+    {
+        $this->directory = null;
+    }
+
+    public function testFileContentsAreReturned()
+    {
+        $testIGCFile = new vfsStreamFile("test.igc");
+
+        $testIGCFile
             ->withContent(self::TEST_CONTENT)
-            ->at($directory);
+            ->at($this->directory);
 
-        $IGCFile = new IGCFile($testIGCFIle->url());
+        $IGCFile = new IGCFile($testIGCFile->url());
 
         $this->assertEquals(self::TEST_CONTENT, $IGCFile->getIGCContents());
+    }
+
+    public function testThrowsExceptionWhenFileDoesntExist()
+    {
+        $this->setExpectedException('InvalidArgumentException');
+
+        new IGCFile($this->directory->url() . "/doesntexist.igc");
     }
 }
